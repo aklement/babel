79a388c11330d7129457299aa100944f3ac06862
Alexandre Klementiev <aklement+github@gmail.com>
2011-03-06 11:49:32 -0500

    Added LSH support and refactored the property classes.

---
+++ src/babel/ranking/scorers/lsh/LSHScorer.java
@@ -0,0 +1,39 @@
+package babel.ranking.scorers.lsh;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import babel.content.eqclasses.EquivalenceClass;
+import babel.content.eqclasses.properties.LSHProperty;
+import babel.ranking.scorers.Scorer;
+import babel.util.jerboa.LSH;
+
+public class LSHScorer extends Scorer {
+  
+  protected static final Log LOG = LogFactory.getLog(LSHScorer.class);
+
+  public LSHScorer(Class<? extends LSHProperty> propClass) {
+    m_propClass = propClass;
+  }
+  
+  public double score(EquivalenceClass srcEq, EquivalenceClass trgEq) {
+    
+    LSHProperty srcProp = (LSHProperty)srcEq.getProperty(m_propClass.getName());     
+    LSHProperty trgProp = (LSHProperty)trgEq.getProperty(m_propClass.getName()); 
+
+    if (srcProp == null || trgProp == null) {
+      throw new IllegalArgumentException("At least one of the classes has no property " + m_propClass.getName() + ".");
+    }
+    
+    return LSH.scoreSignatures(srcProp.getSignature(), trgProp.getSignature());    
+  }
+
+  public void prepare(EquivalenceClass eq) {
+  }
+  
+  public boolean smallerScoresAreBetter() {
+    return false;
+  }
+ 
+  protected Class<? extends LSHProperty> m_propClass;
+}
